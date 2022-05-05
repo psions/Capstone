8b09af9408781d8981afcd12571b7aa7c05d15f1
psions <psionreality@gmail.com>
2022-05-05 15:14:48 -0500

    finished navbar + header with functionality

--- frontend/src/containers/header/Header.jsx
+++ frontend/src/containers/header/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+
 
 const Header = () => {
   return (
-    <div>
-       Header
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Get Your Loot Box Today</h1>
+        <p>Gaming done the right way</p>
 
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder="Your Email Address"/>
+          <button type="button"> Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="people" />
+          <p>1,600 people requested access to visit in last 24 hours</p>
+        </div>
+      </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="ai" />
+        </div>
     </div>
   )
 }
