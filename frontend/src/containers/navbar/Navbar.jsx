0293d5db5b804879bc6cd5cc1420e5f2f231d31b
psions <psionreality@gmail.com>
2022-05-05 12:10:36 -0500

    navbar updates created to give padding and spacing between options on page

--- frontend/src/containers/navbar/Navbar.jsx
+++ frontend/src/containers/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from "../../assets/logo.svg"
 import "./Navbar.css";
 
 // BEM -> Block Element Modifier
@@ -15,6 +16,13 @@ const Navbar = () => {
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wislb">What is a LootBox</a></p>
+          <p><a href="#possibility">Different LootBoxes</a></p>
+          <p><a href="#features">items</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
     </div>
