da5053f2ea0e5d2050cc47235c48ed1f95038568
psions <psionreality@gmail.com>
2022-05-09 15:08:39 -0500

    ui finished with complete functionality

--- frontend/src/containers/footer/Footer.jsx
+++ frontend/src/containers/footer/Footer.jsx
@@ -1,11 +1,49 @@
+import React from "react";
 import "./Footer.css";
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Copyright © 2022</p>{" "}
-    </footer>
-  );
-};
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do You Want To Be Ahead Of The Game</h1>
+      </div>
 
-export default Footer;
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>All Rights Reserved For Gamers</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Social Media</p>
+          <p>Projects</p>
+          <p>World-Wide Shipping</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get In Touch</h4>
+          <p>10236 Charing Cross Road</p>
+          <p>Los Angeles, California</p>
+          <p>1-800-Touch-Grass</p>
+        </div>
+      </div>
+
+      <div className="gpt3__footer-copyright">
+        <p>Did you see the address?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
